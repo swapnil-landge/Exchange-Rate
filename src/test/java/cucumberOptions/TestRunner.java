514325367140aa487ca3316c12ba49ad46fb6c2c
swapnil-landge <swap_lan8@yahoo.co.in>
2021-01-19 13:03:40 +0530

    Update TestRunner.java

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;		
 
 @RunWith(Cucumber.class)				
-@CucumberOptions(features="src/test/java/features",glue={"stepDefinations"})						
+@CucumberOptions(features="src/test/java/features",glue={"stepDefinations", tags = "@UAT"})						
 public class TestRunner 				
 {		
 
